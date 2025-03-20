d033fffe83e43fbb69559c9d6e386d28b1ce36af
Hari prasad <hariprasad9693@gmail.com>
2025-03-20 21:42:22 +0530

    intial commit with cookiecutter template and requirements installed

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='a projrct for classifying comments on Youtube',
+    author='Hari Prasad',
+    license='',
+)
